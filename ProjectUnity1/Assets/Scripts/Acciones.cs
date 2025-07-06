48726b332d895f8cbc7a9c3e78fd9930ab08fc41
Noelia Barchiesi <noelia.barchiesi@hotmail.com>
2025-07-05 22:40:55 -0300

    Prueba Commit

--- ProjectUnity1/Assets/Scripts/Acciones.cs
+++ ProjectUnity1/Assets/Scripts/Acciones.cs
@@ -3,5 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 public interface IAccionContextual
 {
+    // le escribo algo
     void EjecutarAccion(GameObject objetivo, Vector3 destino);
 }
